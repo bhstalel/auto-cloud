b1524f2d7094c15e5815439768d4be9e2b644cb2
bhstalel <bhstalel@gmail.com>
2019-01-23 19:10:23 +0000

    Editing

--- run.sh
+++ run.sh
@@ -3,13 +3,17 @@
 
 clear
 sleep 1
-echo "Nextcloud Auto-Installation"
+echo "+-------------------------------+"
+echo "|                               |"
+echo "|  Nextcloud Auto-Installation  |"
+echo "|      bhstalel@gmail.com       |"
+echo "|                               |"
+echo "+-------------------------------+"
 echo
-read -p "[+] Nginx(1) / Apache2(2): " choice
+read -p "[+] Nginx(1), Apache2(2): " choice
 read -p "[+] Nextcloud username: " username
 read -p "[+] Username password: " userpass
 read -p "[+] Nextcloud domain: " ncdomain
-read -p "[+] Letsencrypt email: " email
 read -p "[+] PHP version: " php_version
 
 echo "[!] Generating config file .."
@@ -18,16 +22,38 @@ touch group_vars/all.yaml
 echo "nextcloud_username: "$username"" > group_vars/all.yaml
 echo "nextcloud_userpass: "$userpass"" >> group_vars/all.yaml
 echo "nextcloud_domain: "$ncdomain"" >> group_vars/all.yaml
-echo "letsencrypt_email: "$email"" >> group_vars/all.yaml
 echo "php_version: "$php_version"" >> group_vars/all.yaml
+echo
 
-echo "[+] Done."
+#####################################
+# Selecting the SSLCertificate option
+#####################################
+read -p "[+] Self-signed-SSLCertificate(1), CA-SSLCertificate(2): " sslchoice
+
+if [ $sslchoice -eq 1 ]; then
+read -p "|__:Hit ENTER to generate:___|" enter0
 echo "[+] Generating auto-signed SSL certificate .."
 sleep 2
 openssl genrsa -out nextcloud.key
 openssl req -new -key nextcloud.key -out d.req -subj "/C=TN/ST=Sousse/L=Sousse/O=Global Security/OU=IT Department/CN=chifco.com"
 openssl x509 -req -days 365 -in d.req -signkey nextcloud.key -out nextcloud.crt
-echo "[!] SSL certificate generated."
-echo "[+] Running the script ... "
-sleep 2
-ansible-playbook playbook.yaml
+
+else
+echo "[-] Coming Soon "
+echo
+fi
+
+if [ $choice -eq 1 ]; then
+	echo "---" > playbook.yaml
+	echo "- hosts: localhost" >> playbook.yaml
+	echo "  roles:" >> playbook.yaml
+	echo "    - nextcloud-nginx" >> playbook.yaml
+	clear
+	echo "[+] SSLCertificate files generated. "
+	echo "[+] Running Nextcloud-Nginx ..."
+	read -p "|___:Hit ENTER to continue:___|" enter
+	sleep 2
+	ansible-playbook playbook.yaml
+else
+	echo "[!] Coming Soon [APACHE2]..."
+fi
