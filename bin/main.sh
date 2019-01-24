3edb52fd8539a8fcda78e5955f64546d3fd73fe9
bhstalel <bhstalel@gmail.com>
2019-01-24 18:50:17 +0000

    More Edit

--- bin/main.sh
+++ bin/main.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-##
-
+#########
+bold="\e[1m"
+underline="\e[4m"
+reset="\e[0m"
+red="\e[31m"
+green="\e[32m"
+blue="\e[34m"
+yellow="\e[33m"
+##########
+cmd=`echo -e "$bold [$green auto-cloud$reset $bold]$ $reset "`
+ch=`echo -e "$bold [$green choice$reset $bold]$ $reset "`
 
 menu(){
 
 echo
-echo " #  Available cmds #   "
+echo -e "$blue$bold #  Available cmds #"
 echo " +=================+ "
 echo " | show            | "
 echo " | add             | "
+echo " | remove          | "
 echo " | reset           | "
+echo " | ready           | "
 echo " | run             | "
 echo " | help            | "
 echo " | exit            | "
-echo " +=================+ "
+echo -e " +=================+ $reset"
 echo
 
 }
@@ -24,69 +35,202 @@ menu
 }
 
 prog=""
-
+PLUGINS=()
 
 showoptions(){
 echo
-echo " #     Plugins     #   "
+echo -e " $blue$bold#     Plugins     #   "
 echo " +=================+ "
 echo " | nextcloud       | "
 echo " | onlyoffice      | "
 echo " | collabora       | "
 echo " | draw            | "
 echo " | ownpad          | "
-echo " +=================+ "
+echo -e " +=================+ $reset"
 echo
 }
 
 
 generatessl(){
 echo
-echo " [NOTE]: You'r installing nextcloud locally, "
-echo "         so we need to generate a self-signed certificate."
+echo -e "$yellow$bold [NOTE]: You'r installing nextcloud locally, "
+echo "         so we need to generate a self-signed certificate.$reset"
 echo
 read -p " + HIT ENTER TO GENERATE + " n
 openssl genrsa -out ../ssl/nextcloud.key
 openssl req -new -key ../ssl/nextcloud.key -out ../ssl/d.req -subj "/C=TN/ST=Sousse/L=Sousse/O=Global Security/OU=IT Department/CN=chifco.com"
 openssl x509 -req -days 365 -in ../ssl/d.req -signkey ../ssl/nextcloud.key -out ../ssl/nextcloud.crt
 echo
-echo " [SSLCertificate] Generated in auto-cloud/ssl/ "
+echo -e " $green$bold [SSLCertificate] Generated in auto-cloud/ssl/ $reset"
 echo
+}
+
+
+# Check for existing NEXTCLOUD plugin
+checknc(){
+for each in "${PLUGINS[@]}"
+do
+	if [ $each = "OFFICIAL-NEXTCLOUD-APACHE" ] || [ $each = "OFFICIAL-NEXTCLOUD-NGINX" ] || [ $each = "LOCAL-NEXTCLOUD-APACHE" ] || [ $each = "LOCAL-NEXTCLOUD-NGINX" ]; then
+		return 1
+	fi
+done
+return 0
 }
 
 add(){
 showoptions
-read -p " [plugin]: " plugin
+echo -e "$bold  ---- $reset"
+echo -e " $red$bold back"
+echo -e "  exit $reset"
+echo
+read -p "$ch" plugin
 	if [ $plugin = "nextcloud" ]; then
-		echo
-		echo " [NEXTCLOUD] chosen."
-		read -p " [ APACHE2(1) , NGINX(2) ]: " web
-		if [ $web -eq 1 ]; then
-			if [ $prog = "o" ]; then
-				echo "    - o-nc-a" >> ../playbook.yaml
+		if checknc; then
+			echo
+			echo -e "$green$bold [NEXTCLOUD] chosen.$reset"
+			echo -e "$bold 1: Apache "
+			echo -e " 2: Nginx $reset"
+			read -p "$ch" web
+			if [ $web -eq 1 ]; then
+				if [ $prog = "o" ]; then
+						echo "    - o-nc-a" >> ../playbook.yaml
+						PLUGINS+=('OFFICIAL-NEXTCLOUD-APACHE')
+				else
+						echo "    - l-nc-a" >> ../playbook.yaml
+						PLUGINS+=('LOCAL-NEXTCLOUD-APACHE')
+				fi
 			else
-				echo "    - l-nc-a" >> ../playbook.yaml
+				if [ $prog = "o" ]; then
+						echo "    - o-nc-n" >> ../playbook.yaml
+						PLUGINS+=('OFFICIAL-NEXTCLOUD-NGINX')
+				else
+						echo "    - l-nc-n" >> ../playbook.yaml
+						PLUGINS+=('LOCAL-NEXTCLOUD-NGINX')
+						generatessl
+				fi
 			fi
+			echo
 		else
-			if [ $prog = "o" ]; then
-				echo "    - o-nc-n" >> ../playbook.yaml
-			else
-				echo "    - l-nc-n" >> ../playbook.yaml
-				generatessl
-			fi
+			echo
+                        echo -e "$red$bold [!] There is NEXTCLOUD plugin exists "
+                        echo -e "     Remove it or continue; $reset"
+                        echo
 		fi
-		echo
+	fi
+	if [ $plugin = "back" ]; then
+		showinput
+	fi
+	if [ $plugin = "exit" ]; then
+		echo -e "$green$bold contact: bhstalel@gmail.com $reset"
+		exit
 	fi
 }
 
 reset(){
+PLUGINS=()
 echo
 echo "---" > ../playbook.yaml
 echo "- hosts: localhost" >> ../playbook.yaml
 echo "  roles:" >> ../playbook.yaml
-echo " [+] Done resting ."
+echo -e "$bold [+] Done reseting .$reset"
 echo
 }
+
+# Printing chosen plugins
+ready(){
+if [ ${#PLUGINS[@]} -eq 0 ]; then
+	echo
+	echo -e "$bold [-] No ready plugins $reset"
+	echo
+else
+	echo
+	echo -e "$yellow$bold [+] Chosen plugins: $reset"
+	printf '%s\n' "  => ${PLUGINS[@]}"
+	echo
+fi
+
+}
+
+# Remove plugin
+remove(){
+if [ ${#PLUGINS[@]} -eq 0 ]; then
+        echo
+        echo -e "$bold [-] Plugins already clean $reset"
+        echo
+else
+        echo
+        echo " [+] Available plugins: "
+        printf '%s\n' "  => ${PLUGINS[@]}"
+        echo
+	read -p "$ch" pl
+	for target in "${pl[@]}"; do
+		for i in "${!PLUGINS[@]}"; do
+			if [[ ${PLUGINS[i]} = $pl ]]; then
+				if [ $pl = "OFFICIAL-NEXTCLOUD-NGINX" ]; then
+					sed -i '/o-nc-n/d' ./../playbook.yaml
+				fi
+
+				if [ $pl = "OFFICIAL-NEXTCLOUD-APACHE" ]; then
+                                        sed -i '/o-nc-a/d' ./../playbook.yaml
+                                fi
+
+				if [ $pl = "LOCAL-NEXTCLOUD-NGINX" ]; then
+                                        sed -i '/l-nc-n/d' ./../playbook.yaml
+                                fi
+
+				if [ $pl = "LOCAL-NEXTCLOUD-APACHE" ]; then
+                                        sed -i '/l-nc-a/d' ./../playbook.yaml
+                                fi
+				unset 'PLUGINS[i]'
+				echo
+				echo -e "$bold [+] $pl removed $reset"
+				echo
+			fi
+		done
+	done
+fi
+}
+
+
+showinput(){
+read -p "$cmd" input
+while [ -z $input ] || [ $input != "exit" ];
+do
+	case $input in
+		"show")
+			showoptions
+			read -p "$cmd" input;;
+		"add")
+			add
+			read -p "$cmd" input;;
+		"remove")
+			remove
+			read -p "$cmd" input;;
+		"ready")
+			ready
+			read -p "$cmd" input;;
+		"reset")
+			reset
+			read -p "$cmd" input;;
+		"run")
+			ansible-playbook ../playbook.yaml
+			read -p "$cmd" input;;
+		"help")
+			help
+			read -p "$cmd" input;;
+		"clear")
+			clear
+			read -p "$cmd" input;;
+		"exit")
+			exit;;
+		*)
+			read -p "$cmd" input;;
+	esac
+
+done
+
+}
+
 
 main(){
 
@@ -94,14 +238,14 @@ if [ $# -eq 0 ]; then
 	prog="o"
 else
 	if [ $# -gt 1 ]; then
-		echo " [-] Invalid number of arguments "
+		echo -e "$red$bold [-] Invalid number of arguments $reset"
 		exit
 	fi
 	if [ $# -eq 1 ]; then
 		if [ $1 = "-l" ]; then
 			prog="l"
 		else
-			echo " [-] Invalid argument "
+			echo -e "$red$bold [-] Invalid argument $reset"
 			exit
 		fi
 	fi
@@ -111,36 +255,7 @@ echo "---" > ../playbook.yaml
 echo "- hosts: localhost" >> ../playbook.yaml
 echo "  roles:" >> ../playbook.yaml
 
-read -p " [auto-cloud]: " input
-while [ -z $input ] || [ $input != "exit" ];
-do
-	case $input in
-		"show")
-			showoptions
-			read -p " [auto-cloud]: " input;;
-		"add")
-			add
-			read -p " [auto-cloud]: " input;;
-		"reset")
-			reset
-			read -p " [auto-cloud]: " input;;
-		"run")
-			ansible-playbook ../playbook.yaml
-			read -p " [auto-cloud]: " input;;
-		"help")
-			help
-			read -p " [auto-cloud]: " input;;
-		"clear")
-			clear
-			read -p " [auto-cloud]: " input;;
-		"exit")
-			exit;;
-		*)
-			read -p " [auto-cloud]: " input;;
-	esac
-
-done
-
+showinput
 }
 
 main $@
