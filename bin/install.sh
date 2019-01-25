4e7aa8f8b9dd2320229469f5ccdd34501152a53c
bhstalel <bhstalel@gmail.com>
2019-01-25 12:14:32 +0000

    edit

--- bin/install.sh
+++ bin/install.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
-
+#########
+bold="\e[1m"
+underline="\e[4m"
+reset="\e[0m"
+red="\e[31m"
+green="\e[32m"
+blue="\e[34m"
+yellow="\e[33m"
+##########
 ## Instaling needs
 
 # Checking ansible
 if hash ansible 2>/dev/null; then
-        echo "[+] Ansible is here "
+        echo -e "$green$bold [+] Ansible is here $reset"
 else
-        echo "[-] We need to install ansible !"
-        echo "[+] Installing ... "
+        echo -e "$yellow$bold [-] We need to install ansible ! $reset"
+        echo -e "$bold [+] Installing ... $reset"
         sleep 1
         sudo apt install software-properties-common
         sudo apt-add-repository ppa:ansible/ansible
         sudo apt-get update
         sudo apt install ansible
-        echo "[+] ansible is installed."
+        echo -e "$green$bold [+] ansible is installed. $reset"
         echo
 fi
 ansible-playbook install.yaml
+clear
+echo -e "$green$bold [+] Installation complete , you can run : bash main.sh --help $reset"
