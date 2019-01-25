c2e8f4d3a369b3c3bc407a11a891bf51a09bde4d
bhstalel <bhstalel@gmail.com>
2019-01-25 12:07:21 +0000

    edit

--- bin/install.sh
+++ bin/install.sh
@@ -16,38 +16,4 @@ else
         echo "[+] ansible is installed."
         echo
 fi
-
-# checking letsencrypt
-if hash letsencrpt 2>/dev/null; then
-                echo "[+] letsencrypt is here "
-else
-                echo "[!] Need to install letsencrypt !"
-                echo "[+] Installing ..."
-                sudo apt install letsencrypt -y
-                echo "[+] letsencrypt is installed."
-fi
-
-
-# checking python-certbot-nginx
-if hash python-certbot-nginx 2>/dev/null; then
-                echo "[+] python-certbot-nginx is here "
-else
-                echo "[!] Need to install python-certbot-nginx !"
-                echo "[+] Installing ..."
-                sudo add-apt-repository ppa:certbot/certbot
-                sudo apt-get update
-                sudo apt-get install python-certbot-nginx
-                echo "[+] python-certbot-nginx is installed."
-fi
-
-
-# checking python-certbot-apache
-if hash python-certbot-apache 2>/dev/null; then
-                echo "[+] python-certbot-apache is here "
-else
-                echo "[!] Need to install python-certbot-apache !"
-                echo "[+] Installing ..."
-                sudo apt-get install python-certbot-apache
-                echo "[+] python-certbot-apache is installed."
-fi
-
+ansible-playbook install.yaml
