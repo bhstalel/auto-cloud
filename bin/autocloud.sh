0e409e0aef05d8db735d37bdc986d96a6d1158dd
bhstalel <bhstalel@gmail.com>
2019-02-02 11:59:23 +0100

    EDIT

--- bin/autocloud.sh
+++ bin/autocloud.sh
@@ -17,47 +17,87 @@ pressenter=`echo -e "$bold [PRESS ENTER TO CONTINUE] $reset"`
 # [       HELP FONCTION        ]
 HELPMENU(){
 echo
-echo -e "$green$bold [ Available arguments ] $reset"
-echo -e "$bold # help "
-echo -e "$bold # list "
-echo -e "$bold # init "
-echo -e "$bold # config "
-echo -e "$bold # plugins plug1,plug2,..       [FOR OFFICIAL DOMAIN   ]"
-echo -e "$bold # local plugins plug1,plug2,.. [FOR LOCAL INSTALLATION]"
-echo -e "$bold # set var value "
-echo -e "$bold +===+"
-echo -e "$bold   vars: "
-echo -e "$bold         - ncdbuser "
-echo -e "$bold         - ncdbpass "
-echo -e "$bold         - ncdm "
-echo -e "$bold         - oodm "
-echo -e "$bold         - codm "
-echo -e "$bold         - email "
-echo -e "$bold         - webserver "
-echo -e "$bold         - dbtype"
-echo -e "$bold +===+"
+echo -e "$bold +==========# ARGS #==========+ "
+echo -e "$bold |                            |"
+echo -e "$bold |  + help                    |  $blue[show this menu]$reset"
+echo -e "$bold |                            |" 
+echo -e "$bold |  + list                    |  $blue[list plugins]$reset"
+echo -e "$bold |                            |"
+echo -e "$bold |  + init                    |  $blue[reset variables]$reset"
+echo -e "$bold |                            |"
+echo -e "$bold |  + config                  |  $blue[show variables]$reset"
+echo -e "$bold |                            |"
+echo -e "$bold |  + plugins plug1,plug2,..  |  $blue[run plug1,plug2,..]$reset"
+echo -e "$bold |                            |"
+echo -e "$bold |  + set var value           |  $blue[edit var's value]$reset"
+echo -e "$bold |                            |"
+echo -e "$bold +==========# VARS #==========+ "
+echo -e "$bold |                            |"
+echo -e "$bold |  + state                   | $blue [l=local,o=official]$reset"
+echo -e "$bold |                            |"
+echo -e "$bold |  + ncdbuser                | $blue [nextcloud_username]$reset"
+echo -e "$bold |                            |"
+echo -e "$bold |  + ncdbpass                | $blue [nextcloud_userpass]$reset"
+echo -e "$bold |                            |"
+echo -e "$bold |  + ncdm                    | $blue [nextcloud_domain]$reset"
+echo -e "$bold |                            |"
+echo -e "$bold |  + oodm                    | $blue [onlyoffice_domain]$reset"
+echo -e "$bold |                            |"
+echo -e "$bold |  + codm                    | $blue [collabora_domain]$reset"
+echo -e "$bold |                            |"
+echo -e "$bold |  + email                   | $blue [email]$reset"
+echo -e "$bold |                            |"
+echo -e "$bold |  + webserver               | $blue [a=apache,n=nginx]$reset"
+echo -e "$bold |                            |"
+echo -e "$bold |  + dbtype                  | $blue [m=mysql,p=postgresql]$reset"
+echo -e "$bold |                            |"
+echo -e "$bold +============================+"
 echo -e "$reset"
 }
 
 # +============================+
 # [ Show available plugins     ]
 SHOWPLUGINS(){
-file="../group_vars/plugins"
 echo
-echo -e "$green$bold [ Available plugins ] $reset"
-while IFS= read line
-do
-	echo -e "$bold # $line $reset"
-done <"$file"
+echo -e "$bold +=========# PLUGS #==========+"
+echo -e "$bold |                            |"
+echo -e "$bold |  + nextcloud               |"
+echo -e "$bold |                            |"
+echo -e "$bold |  + onlyoffice              |"
+echo -e "$bold |                            |"
+echo -e "$bold |  + collabora               |"
+echo -e "$bold |                            |"
+echo -e "$bold |  + Talk                    |"
+echo -e "$bold |                            |"
+echo -e "$bold |  + Draw                    |"
+echo -e "$bold |                            |"
+echo -e "$bold |  + Ownpad                  |"
+echo -e "$bold |                            |"
+echo -e "$bold +============================+$reset"
 echo
 }
 
 # +============================+
 # [ Initialise all variables   ]
 INITFILE(){
-echo
-echo -e "$bold [#] Done initialising $reset"
-echo
+newline="state:"
+sed -i '/state.*/c\'"$newline" ../group_vars/all.yaml
+newline="nextcloud_username:"
+sed -i '/nextcloud_username.*/c\'"$newline" ../group_vars/all.yaml
+newline="nextcloud_userpass:"
+sed -i '/nextcloud_userpass.*/c\'"$newline" ../group_vars/all.yaml
+newline="nextcloud_domain:"
+sed -i '/nextcloud_domain.*/c\'"$newline" ../group_vars/all.yaml
+newline="onlyoffice_domain:"
+sed -i '/onlyoffice_domain.*/c\'"$newline" ../group_vars/all.yaml
+newline="collabora_domain:"
+sed -i '/collabora_domain.*/c\'"$newline" ../group_vars/all.yaml
+newline="webserver:"
+sed -i '/webserver.*/c\'"$newline" ../group_vars/all.yaml
+newline="email:"
+sed -i '/email.*/c\'"$newline" ../group_vars/all.yaml
+newline="dbtype:"
+sed -i '/dbtype.*/c\'"$newline" ../group_vars/all.yaml
 }
 
 
@@ -65,53 +105,81 @@ echo
 # [       Show variables       ]
 SHOWCONF(){
 echo
-echo -e "$green$bold [ Variables ] $reset"
-
+echo -e "$bold +=========# VARS #=======+=========# VALUES #=======+ $reset"
+echo -e "$bold |                        |"
+state=`cat ../group_vars/all.yaml | sed -n -e 's/^.*state: //p'`
+if [ -z "$state" ]; then
+	echo -e "$bold | + state                :         $red[NOT SET]$reset"
+else
+	echo -e "$bold | + state                :         $blue$state$reset"
+fi
+echo -e "$bold |                        |"
 ncdbuser=`cat ../group_vars/all.yaml | sed -n -e 's/^.*nextcloud_username: //p'`
 if [ -z "$ncdbuser" ]; then
-	echo -e "$bold # nextcloud_username: $red[NOT SET]$reset"
+	echo -e "$bold | + nextcloud_username   :         $red[NOT SET]$reset"
 else
-	echo -e "$bold # nextcloud_username: $blue$ncdbuser$reset"
+	echo -e "$bold | + nextcloud_username   :         $blue$ncdbuser$reset"
 fi
 
+echo -e "$bold |                        |"
 ncdbpass=`cat ../group_vars/all.yaml | sed -n -e 's/^.*nextcloud_userpass: //p'`
 if [ -z "$ncdbpass" ]; then
-	echo -e "$bold # nextcloud_userpass: $red[NOT SET]$reset"
+	echo -e "$bold | + nextcloud_userpass   :         $red[NOT SET]$reset"
 else
-	echo -e "$bold # nextcloud_userpass: $blue$ncdbpass$reset"
+	echo -e "$bold | + nextcloud_userpass   :         $blue$ncdbpass$reset"
 fi
 
+echo -e "$bold |                        |"
 ncdm=`cat ../group_vars/all.yaml | sed -n -e 's/^.*nextcloud_domain: //p'`
 if [ -z "$ncdm" ]; then
-	echo -e "$bold # nextcloud_domain: $red[NOT SET]$reset"
+	echo -e "$bold | + nextcloud_domain     :         $red[NOT SET]$reset"
 else
-	echo -e "$bold # nextcloud_domain: $blue$ncdm$reset"
+	echo -e "$bold | + nextcloud_domain     :         $blue$ncdm$reset"
 fi
 
+echo -e "$bold |                        |"
 oodm=`cat ../group_vars/all.yaml | sed -n -e 's/^.*onlyoffice_domain: //p'`
 if [ -z "$oodm" ]; then
-	echo -e "$bold # onlyoffice_domain: $red[NOT SET]$reset"
+	echo -e "$bold | + onlyoffice_domain    :         $red[NOT SET]$reset"
 else
-	echo -e "$bold # onlyoffice_domain: $blue$oodm$reset"
+	echo -e "$bold | + onlyoffice_domain    :         $blue$oodm$reset"
 fi
 
+echo -e "$bold |                        |"
 codm=`cat ../group_vars/all.yaml | sed -n -e 's/^.*collabora_domain: //p'`
 if [ -z "$codm" ]; then
-	echo -e "$bold # collabora_domain: $red[NOT SET]$reset"
+	echo -e "$bold | + collabora_domain     :         $red[NOT SET]$reset"
 else
-	echo -e "$bold # collabora_domain: $blue$codm$reset"
+	echo -e "$bold | + collabora_domain     :         $blue$codm$reset"
 fi
 
+echo -e "$bold |                        |"
 email=`cat ../group_vars/all.yaml | sed -n -e 's/^.*email: //p'`
 if [ -z "$email" ]; then
-	echo -e "$bold # email: $red[NOT SET]$reset"
+	echo -e "$bold | + email                :         $red[NOT SET]$reset"
 else
-	echo -e "$bold # email: $blue$email$reset"
+	echo -e "$bold | + email                :         $blue$email$reset"
 fi
 
-echo -e "$bold # webserver:  $red[NOT SET]$reset"
-echo -e "$bold # dbtype:     $red[NOT SET]$reset $reset"
+echo -e "$bold |                        |"
+webserv=`cat ../group_vars/all.yaml | sed -n -e 's/^.*webserver: //p'`
+if [ -z "$webserv" ]; then
+	echo -e "$bold | + webserver            :         $red[NOT SET]$reset"
+else
+	echo -e "$bold | + webserver            :         $blue$webserv$reset"
+fi
+
+echo -e "$bold |                        |"
+dbtype=`cat ../group_vars/all.yaml | sed -n -e 's/^.*dbtype: //p'`
+if [ -z "$dbtype" ]; then
+	echo -e "$bold | + dbtype               :         $red[NOT SET]$reset $reset"
+else
+	echo -e "$bold | + dbtype               :         $blue$dbtype$reset"
+fi
+echo -e "$bold |                        |"
+echo -e "$bold +========================+==========================+ $reset"
 echo
+
 }
 
 # +============================+
@@ -140,45 +208,74 @@ echo
 }
 
 # +============================+
+# [      VAR NOT FOUND         ]
+VARNOTFOUND(){
+echo
+echo -e "$bold ["$1"] Not found $reset"
+echo
+}
+
+# +============================+
+# [ Set: state [local/official ] 
+SETSTATE(){
+newline="state: $1"
+sed -i '/state.*/c\'"$newline" ../group_vars/all.yaml
+}
+
+# +============================+
 # [ Set: nextcloud_username    ]
 SETNCDBUSER(){
-echo "nextcloud_username: $1"
+newline="nextcloud_username: $1"
+sed -i '/nextcloud_username.*/c\'"$newline" ../group_vars/all.yaml
 }
 
 # +============================+
 # [ Set: nextcloud_userpass    ]
 SETNCDBPASS(){
-echo "nextcloud_userpass: $1"
+newline="nextcloud_userpass: $1"
+sed -i '/nextcloud_userpass.*/c\'"$newline" ../group_vars/all.yaml
 }
 
 # +============================+
 # [ Set: nextcloud_domain      ]
 SETNCDM(){
-echo "nextcloud_domain: $1"
+newline="nextcloud_domain: $1"
+sed -i '/nextcloud_domain.*/c\'"$newline" ../group_vars/all.yaml
 }
 
 # +============================+
 # [ Set: onlyoffice_domain     ]
 SETOODM(){
-echo "onlyoffice_domain: $1"
+newline="onlyoffice_domain: $1"
+sed -i '/onlyoffice_domain.*/c\'"$newline" ../group_vars/all.yaml
 }
 
 # +============================+
 # [ Set: collabora_domain      ]
 SETCODM(){
-echo "collabora_domain: $1"
+newline="collabora_domain: $1"
+sed -i '/collabora_domain.*/c\'"$newline" ../group_vars/all.yaml
 }
 
 # +============================+
 # [ Set: webserver             ]
 SETWEBSERV(){
-echo "webserver: $1"
+newline="webserver: $1"
+sed -i '/webserver.*/c\'"$newline" ../group_vars/all.yaml
 }
 
 # +============================+
 # [ Set: email                 ]
 SETEMAIL(){
-echo "email: $1"
+newline="email: $1"
+sed -i '/email.*/c\'"$newline" ../group_vars/all.yaml
+}
+
+# +============================+
+# [ Set: dbtype                ]
+SETDBTYPE(){
+newline="dbtype: $1"
+sed -i '/dbtype.*/c\'"$newline" ../group_vars/all.yaml
 }
 
 
@@ -231,6 +328,7 @@ elif [ $# -eq 3 ]; then
 		fi
 	elif [ "$1" = "set" ]; then
 		case "$2" in 
+			"state") SETSTATE "$3";;
 			"ncdbuser") SETNCDBUSER "$3";;
 			"ncdbpass") SETNCDBPASS "$3";;
 			"ncdm") SETNCDM "$3";;
@@ -238,7 +336,8 @@ elif [ $# -eq 3 ]; then
 			"codm") SETCODM "$3";;
 			"email") SETEMAIL "$3";;
 			"webserv") SETWEBSERV "$3";;
-			*) ARGERROR;;
+			"dbtype") SETDBTYPE "$3";;
+			*) VARNOTFOUND "$2" ;;
 		esac
 	else
 		ARGERROR
